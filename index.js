7ff090f50c0a414bcb4a85193a366eb62421acb5
Hiep Nguyen <hiepnh610@gmail.com>
2019-09-10 22:04:18 +0700

    handle code for compare

--- index.js
+++ index.js
@@ -4,26 +4,60 @@ const http      = require('http').Server(app);
 const XLSX      = require('xlsx');
 const fs        = require('fs');
 const jsdom     = require("jsdom");
+const _         = require('lodash');
 const { JSDOM } = jsdom;
 
+const nameFile = '91091186';
 
-const excelFile = XLSX.readFile('xlsx/91091185.xlsx');
+const excelFile = XLSX.readFile(`xlsx/${nameFile}.xlsx`);
 const worksheet = excelFile.Sheets[excelFile.SheetNames[0]];
 
-const columnC = [];
+const getDataFromExcelFile = () => {
+  let column = [];
 
-for (let z in worksheet) {
-  if(z.toString() !== 'C1' && z.toString()[0] === 'C') {
-    columnC.push(worksheet[z].v);
+  for (let z in worksheet) {
+    if(z.toString() !== 'C1' && z.toString()[0] === 'C') {
+      column.push(worksheet[z].v);
+    }
   }
-}
 
-fs.readFile('./svg/91091185.svg', (err, data) => {
-  if (err) throw err;
+  return column;
+};
 
-  const dom = new JSDOM(data);
-  console.log(dom.window.document.getElementsByTagName('g'));
-});
+const getDataFromSVGFile = () => {
+  const listCode = getDataFromExcelFile();
+
+  fs.readFile(`./svg/${nameFile}.svg`, (err, data) => {
+    if (err) throw err;
+
+    let seatCode = [];
+
+    const dom = new JSDOM(data);
+    const getListOfSeats = dom.window.document.getElementsByTagName('g');
+
+    for (seat of getListOfSeats) {
+      const newSeat = new JSDOM(seat.outerHTML, { includeNodeLocations: true });
+      const getSeatCode = newSeat.window.document.querySelector('g').getAttribute('id');
+
+      // console.log(getSeatCode);
+
+      if (getSeatCode) {
+        seatCode.push(getSeatCode);
+      }
+    }
+
+    // console.log('listCode', listCode.length);
+    // console.log('seatCode', seatCode.length);
+
+    // if (seatCode.length === listCode.length) {
+    //   console.log(_.difference(seatCode, listCode));
+    // }
+
+    console.log(_.difference(seatCode, listCode));
+  });
+};
+
+getDataFromSVGFile();
 
 http.listen(3000, () => {
   console.log(`This app listen on port 3000`);
